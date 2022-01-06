5d7d51f936d9e6947142d93dd52a645ee4649767
Celine Joie V. Buenaventura <buenaventuraceline277@gmail.com>
2022-01-06 13:27:23 +0800

    Step 3 Part B

--- lib/quiz_brain.dart
+++ lib/quiz_brain.dart
@@ -51,6 +51,7 @@ class QuizBrain {
 
   bool isFinished() {
     if (_questionNumber >= _questionBank.length - 1) {
+      print('Now returning true');
       return true;
     } else {
       return false;
